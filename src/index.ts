39fcd13acc568734887ae74d0b1e6fc369eb450d
Kamrulthedev <kamrulthedevR@gmail.com>
2025-08-02 16:29:31 +0600

    add-data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-const MyName: string = "Kamrul Hassan Jinuk Hi";
-const myage : number = 25;
-console.log("Hello, This is My Name: ", MyName);
+const data = {
+    name : "Kamrul Hassan Jinuk",
+    age : 24,
+    profession : "Software Engineer",
+    Location : "Cox`s Bazar, Chattogram, Bangladesh",
+    hobbies: ["Coding", "Reading Books", "Travelling", "Playing Daba", "pricticing English"],
+    Skills : ["JavaScript", "TypeScript", "React", "Next.js", "Node.js", "Express.js", "GrapjQl", "Prisma", "PostgreSQL", "MongoDB"], 
+}
+
+
+
+
+
+console.log("Hello, This is My Name: ", data);
 
