306ef83cc7c74454434f0ef89ba9022fccaff47b
Kamrulthedev <kamrulthedevR@gmail.com>
2025-08-03 12:33:55 +0600

    create-mutation-resolvers

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,5 +2,12 @@ export const resolvers = {
   Query: {
   
   },
+
+  Mutation : {
+    signUp : (parent: any, args: any, content: any) => {
+      console.log("Sign Up Mutation called with args : ", args);
+    }
+  }
+
 };
 
