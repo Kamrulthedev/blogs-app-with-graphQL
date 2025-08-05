645d5c7b6d3f941830574150126ca35df4931c16
Kamrulthedev <kamrulthedevR@gmail.com>
2025-08-05 21:58:28 +0600

    suggention-console

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -5,6 +5,7 @@ export const resolvers = {
 
   Mutation : {
     signUp : (parent: any, args: any, content: any) => {
+      
       console.log("Sign Up Mutation called with args : ", args);
     }
   }
