96fb3eda2d565d37c8da3dfdf12a93a4fc0aa7be
Kamrulthedev <kamrulthedevR@gmail.com>
2025-08-07 12:49:27 +0600

    install-dot-env

--- src/utils/tokenHelper.ts
+++ src/utils/tokenHelper.ts
@@ -4,3 +4,4 @@ export const tokenHelper = async (payload: { userId: number }) => {
   const token = jwt.sign(payload, process.env.jwtSecret || "kamrul1234567899", { expiresIn: "1d" });
   return token;
 };
+
