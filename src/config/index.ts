2906bf53a38afad6f68c5071b195879c5dbb3d5d
Kamrulthedev <kamrulthedevR@gmail.com>
2025-08-07 13:09:48 +0600

    update-code

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,10 @@
+import { Secret } from "jsonwebtoken";
 import path from "path";
 
 require("dotenv").config({path: path.join(process.cwd(), ".env")});
 
 export default {
     jwt :{
-       secret : process.env.jwtSecret
+       secret : process.env.jwtSecret as Secret
     }
 };
--- src/utils/tokenHelper.ts
+++ src/utils/tokenHelper.ts
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken"
+import config from "../config";
 require("dotenv").config();
 
 export const tokenHelper = async (payload: { userId: number }) => {
-  const token = jwt.sign(payload, process.env.jwtSecret || "kamrul1234567899", { expiresIn: "1d" });
+  const token = jwt.sign(payload, config.jwt.secret, { expiresIn: "1d" });
   return token;
 };
 
