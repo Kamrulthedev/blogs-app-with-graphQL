76393d37f22973812b508ad807cb1dfdc574ef10
Kamrulthedev <kamrulthedevR@gmail.com>
2025-08-18 00:56:26 +0600

    prisma-type-set

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema.js';
 import { resolvers } from './resolvers/index.js';
-import { PrismaClient } from "@prisma/client";
-
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from '@prisma/client/runtime/library.js';
 
 
 const prisma = new PrismaClient();
+
+interface Context {
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>
+};
 
 const server = new ApolloServer({
   typeDefs,
@@ -16,7 +20,7 @@ const server = new ApolloServer({
 async function bootstrap() {
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4001 },
-    context: async() =>{
+    context: async(): Promise<Context> =>{
       return {
         prisma
       }
