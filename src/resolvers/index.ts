394c7dbb57f84d52402be6c49e4f853b875152b7
Kamrulthedev <kamrulthedevR@gmail.com>
2025-08-18 00:51:28 +0600

    resolvaer-code-orgonaize

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,9 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import { Query } from "./Querys/index.js";
 import { Mutation } from "./Mutations/index.js";
-
-const prisma = new PrismaClient();
-
 
 
 export const resolvers = {
