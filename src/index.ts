b2ea96dd9b5b05518adb2dd63408f4bafcd60424
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-09 15:14:04 +0600

    use try catch

--- src/index.ts
+++ src/index.ts
@@ -24,17 +24,23 @@ const server = new ApolloServer({
 async function bootstrap() {
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4002 },
-    context: async({req}): Promise<Context> =>{
-      console.log(req.headers.authorization)
-      
-      // Decoded with function
-      // const decodedToken = await JwtHelper.DecodeToken(req.headers.authorization as string); 
+    context: async ({ req }): Promise<Context> => {
+      try {
+        console.log(req.headers.authorization)
+        // Decoded with function
+        // const decodedToken = await JwtHelper.DecodeToken(req.headers.authorization as string); 
 
-      // Decoded Menual
-      const decodedToken = jwt.verify(req.headers.authorization as string, "kamrul1234567899");
-      console.log(decodedToken)
-      return {
-        prisma
+        // Decoded Menual
+        const decodedToken = jwt.verify(req.headers.authorization as string, "kamrul1234567899");
+        console.log("User data", decodedToken)
+
+        return {
+          prisma
+        }
+      }
+      catch (error : any) {
+        console.error("Error verifying token:", error.message);
+        throw new Error("Unauthorized: Invalid or missing token");
       }
     }
   });
