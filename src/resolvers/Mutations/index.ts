f5749f606492b97a7f4e62101965682bf1e6024c
Kamrulthedev <kamrulthedevR@gmail.com>
2025-12-04 11:10:00 +0600

    to show where the warning was created

--- src/resolvers/Mutations/index.ts
+++ src/resolvers/Mutations/index.ts
@@ -1,49 +1,9 @@
 import { authResolvers } from "./auth.js";
+import { PostResolvers } from "./post.js";
 
 export const Mutation = {
 ...authResolvers,
-
-  // Create Post Mutation
-  createPost: async (parent: any, args: any, { prisma, decodedToken }: any) => {
-    // console.log("Data:", args);
-    // console.log("Decoded Token:", decodedToken);
-
-    // Check if the author exists
-    if (!decodedToken || !decodedToken.userId) {
-      return {
-        userError: "Forbidden Access!",
-        post: null,
-      };
-    }
-
-    // Check title and content
-    const { title, content } = args;
-    if (!title || !content) {
-      return {
-        userError: "Title And Content Must Be Provided!",
-        post: null,
-      };
-    }
-
-    // Create the Post
-    const post = await prisma.post.create({
-      data: {
-        title,
-        content,
-        authorId: decodedToken.userId,
-      },
-      include: {
-        author: true,
-      },
-    });
-
-    // console.log("Post Created:", post);
-    return {
-      userError: null,
-      post: post
-    };
-  }
-
+...PostResolvers
 
   // add more mutations here as needed
 
