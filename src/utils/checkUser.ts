da2942dc1042f8362346e6e01ef71fc803964de7
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-30 20:48:56 +0600

    check user commit

--- src/utils/checkUser.ts
+++ src/utils/checkUser.ts
@@ -1,3 +1,40 @@
 const CheckUserAccess = async () => {
+    
+    // Check if the author exists
+    if (!decodedToken || !decodedToken.userId) {
+        return {
+            userError: "Unauthorized Access!",
+            post: null
+        }
+    };
+
+    // Check token Auth Id Exists Post Author Id 
+    const user = await prisma.user.findUnique({
+        where: { id: decodedToken.userId }
+    });
+
+    if (!user) {
+        return {
+            userError: "User Not Found!"
+        }
+    }
+
+    // Check Post ID Exists
+    const existsPost = await prisma.post.findUnique({
+        where: { id: Number(postId) }
+    })
+    if (!existsPost) {
+        return {
+            userError: "Post Not Found!",
+            post: null
+        }
+    }
+
+    // Check Author Id and User Id Match
+    if (existsPost.authorId !== user.id) {
+        return {
+            userError: "Post Not Wound By User!"
+        }
+    }
 
 }
