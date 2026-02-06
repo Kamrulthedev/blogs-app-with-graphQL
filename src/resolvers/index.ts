7dcecbb772296cad72949564b159ceabf1dd8cd2
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-06 16:37:58 +0600

    install post file

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,11 +1,12 @@
 import { Query } from "./Querys/index.js";
 import { Mutation } from "./Mutations/index.js";
+import { Post } from "./post.js";
 
 
 export const resolvers = {
   Query,
   Mutation,
-  
+  Post
 };
 
 
