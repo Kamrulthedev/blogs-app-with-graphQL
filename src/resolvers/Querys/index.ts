d720aa82e3687522eb2536e837c1748d6bd67712
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-06 14:54:31 +0600

    return user

--- src/resolvers/Querys/index.ts
+++ src/resolvers/Querys/index.ts
@@ -38,11 +38,16 @@ export const Query = {
 
     // Query to get the current authenticated user
     me: async (parent: any, args: any, { prisma, decodedToken }: any) => {
-        // Check if the user is authenticated
-        if (!decodedToken || !decodedToken.userId) {
-            return null;
-        }
-        
+        // // Check if the user is authenticated
+        // if (!decodedToken || !decodedToken.userId) {
+        //     return null;
+        // }
+
+        const user = await prisma.user.findUnique({
+            where: { id: decodedToken.userId },
+            include: { posts: true }
+        })
+        return user;
     }
 
 
