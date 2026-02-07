25cd621a6e30a82141c9509c300f9c80abc67d55
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-07 11:40:19 +0600

    Check if the user is trying to access their own profile

--- src/resolvers/users.ts
+++ src/resolvers/users.ts
@@ -1,5 +1,9 @@
 export const User = {
     posts: async (parent: any, args: any, {prisma, decodedToken}: any) =>{
+        
+        // Check if the user is trying to access their own profile
+        const ifMyProfile = parent.id === decodedToken.userId;
+
         const posts =  await prisma.post.findMany({
             where: {
                 authorId: parent.id
