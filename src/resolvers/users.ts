c5084303525fc1f305699866002e3791a55163ea
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-07 11:50:43 +0600

    chack my profile and others profile

--- src/resolvers/users.ts
+++ src/resolvers/users.ts
@@ -1,14 +1,34 @@
 export const User = {
-    posts: async (parent: any, args: any, {prisma, decodedToken}: any) =>{
-        
+    posts: async (parent: any, args: any, { prisma, decodedToken }: any) => {
+        console.log(parent, decodedToken)
+
         // Check if the user is trying to access their own profile
         const ifMyProfile = parent.id === decodedToken.userId;
+        console.log("Is My Profile:", ifMyProfile)
 
-        const posts =  await prisma.post.findMany({
+        // If it`s their own profile, return all posts (including unpublished)
+        if(ifMyProfile){
+            return await prisma.post.findMany({
+                where: {
+                    authorId: parent.id
+                }
+            })
+        }
+        else{
+            return await prisma.post.findMany({
+                where: {
+                    authorId: parent.id,
+                    published: true
+                }
+            })
+        }
+
+        const posts = await prisma.post.findMany({
             where: {
                 authorId: parent.id
             }
         })
+
         return posts;
     }
 };
