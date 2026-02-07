838ff166bdc8e95b80e5c68a60014f8695ab4ddc
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-07 10:00:12 +0600

    not console commit

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,12 +1,8 @@
 export const Post = {
     author: async (parent: any, args: any, { prisma }: any) => {
-        console.log("Post Author Resolver Called With Parent:", parent);
         const author = await prisma.user.findUnique({
             where: { id: parent.authorId }
         })
-
-        console.log("Author Found:", author)
-
         return author;
     }
-}
+};
