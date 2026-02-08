047ebcc21050bf9e30ab5c56804936f1ad3cc193
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-08 14:00:24 +0600

    update code

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,13 +2,15 @@ import { Query } from "./Querys/index.js";
 import { Mutation } from "./Mutations/index.js";
 import { Post } from "./post.js";
 import { User } from "./users.js";
+import { Profile } from "./profile.js";
 
 
 export const resolvers = {
   Query,
   Mutation,
   Post,
-  User
+  User,
+  Profile
 };
 
 
