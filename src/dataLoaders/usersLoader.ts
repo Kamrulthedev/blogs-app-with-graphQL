36f9b9307f1ed0254e7cc1438b5e37cb6fa74267
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-11 00:20:37 +0600

    format code

--- src/dataLoaders/usersLoader.ts
+++ src/dataLoaders/usersLoader.ts
@@ -1,5 +1,5 @@
 import { User } from "@prisma/client";
 
-const batchUsers = async (userIds: number[]) : Promise<User[]> =>{
+const batchUsers = async (userIds: number[]): Promise<User[]> => {
 
 };
