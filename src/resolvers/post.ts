e0d7adb327a9ab918b4e997c70dd94b6fd9ff4c4
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-10 23:59:07 +0600

    console with post user id

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,6 @@
 export const Post = {
     author: async (parent: any, args: any, { prisma }: any) => {
+        console.log("User Id:", parent.authorId)
         const author = await prisma.user.findUnique({
             where: { id: parent.authorId }
         })
