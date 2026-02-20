466bddc1b4b8a8c9e9512a72a6c33eba0c4541f2
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-21 00:40:50 +0600

    create a new function batch users commit

--- src/dataLoaders/usersLoader.ts
+++ src/dataLoaders/usersLoader.ts
@@ -2,10 +2,6 @@ import { Prisma, User } from "@prisma/client";
 
 const batchUsers = async (userIds: number[]): Promise<User[]> => {
 
-    console.log("Batching user IDs:" , userIds);
-
-    const users= await Prisma.
 
 };
-
 
