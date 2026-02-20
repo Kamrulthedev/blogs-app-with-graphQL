fffdb15e35dd15896e07f6c99e3c1abceb8daa98
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-21 01:16:51 +0600

    const user wrtie

--- src/dataLoaders/usersLoader.ts
+++ src/dataLoaders/usersLoader.ts
@@ -1,8 +1,15 @@
 import { Prisma, User } from "@prisma/client";
+import { prisma } from "..";
 
 const batchUsers = async (Ids: number[]): Promise<User[]> => {
 
-    
+    const users = await prisma.user.findMany({
+        where: {
+            id: {
+                in: Ids
+            }
+        }
+    })
 
 };
 
