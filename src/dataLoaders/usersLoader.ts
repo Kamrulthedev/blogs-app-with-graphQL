64adf3f16f4932796c5cf6cd37a156ce1a243d77
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-02-21 01:15:46 +0600

    export prisma

--- src/dataLoaders/usersLoader.ts
+++ src/dataLoaders/usersLoader.ts
@@ -2,6 +2,7 @@ import { Prisma, User } from "@prisma/client";
 
 const batchUsers = async (Ids: number[]): Promise<User[]> => {
 
+    
 
 };
 
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import config from './config/index.js';
 
 
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 interface Context {
   prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>
